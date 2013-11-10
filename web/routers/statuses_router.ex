9731c6310b7167d398835594e7f057ef453967cd
Daniel Farrell <danielfarrell76@gmail.com>
2013-11-10 11:12:27 -0500

    Make sure you can't manually post to /statuses without being logged in

--- web/routers/statuses_router.ex
+++ web/routers/statuses_router.ex
@@ -12,6 +12,7 @@ defmodule StatusesRouter do
     render conn, "statuses/new.html"
   end
 
+  @prepare :authenticate_user
   post "/" do
     session = get_session(conn)
     status = Status.new(text: conn.params[:text], user_id: session[:user_id], created_at: Now.datetime, updated_at: Now.datetime)
