874dc6e19fa4ce93313acf34acf36b7ac910c99d
Daniel Farrell <danielfarrell76@gmail.com>
2013-11-10 11:47:30 -0500

    Clean up status display and use a partial

--- lib/string.chars.ex
+++ lib/string.chars.ex
@@ -4,6 +4,7 @@ defimpl String.Chars, for: Ecto.DateTime do
     day = String.rjust("#{d.day}", 2, ?0)
     hour = String.rjust("#{d.hour}", 2, ?0)
     min = String.rjust("#{d.min}", 2, ?0)
-    "#{d.year}-#{month}-#{day} #{hour}:#{min}"
+    sec = String.rjust("#{d.sec}", 2, ?0)
+    "#{d.year}-#{month}-#{day}T#{hour}:#{min}:#{sec}-05:00"
   end
 end
