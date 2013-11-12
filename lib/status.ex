fbf524a1a6e2c560fb7a833037f5fbe546e7c2d6
Daniel Farrell <danielfarrell76@gmail.com>
2013-11-11 20:08:13 -0500

    Move statuses module to it's own file

--- lib/status.ex
+++ lib/status.ex
@@ -7,32 +7,3 @@ defmodule Status do
     belongs_to :user, User
   end
 end
-
-defmodule Statuses do
-  import Ecto.Query
-
-  @per_page 30
-  def recent(page // 1) do
-    query = from s in Status,
-      order_by: [desc: s.created_at],
-      limit: @per_page,
-      offset: skip(page),
-      preload: [:user]
-    Repo.all(query)
-  end
-
-  def for_user(user_id, page // 1) do
-    query = from s in Status,
-      where: s.user_id == ^user_id,
-      order_by: [desc: s.created_at],
-      limit: @per_page,
-      offset: skip(page),
-      preload: [:user]
-    Repo.all(query)
-  end
-
-  defp skip(1), do: 0
-  defp skip(page) when page < 1, do: 0
-  defp skip(page), do: (page - 1) * @per_page
-
-end
