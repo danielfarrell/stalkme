3ba96f144c655835d23670c77b951247d893d326
Daniel Farrell <danielfarrell76@gmail.com>
2013-11-21 12:17:33 -0500

    Switch to universal time

--- lib/now.ex
+++ lib/now.ex
@@ -1,6 +1,6 @@
 defmodule Now do
   def datetime do
-    {{year, month, day},{hour, min, sec}} = :erlang.localtime
+    {{year, month, day},{hour, min, sec}} = :erlang.universaltime
     Ecto.DateTime[year: year, month: month, day: day, hour: hour, min: min, sec: sec]
   end
 end
--- lib/string.chars.ex
+++ lib/string.chars.ex
@@ -5,6 +5,6 @@ defimpl String.Chars, for: Ecto.DateTime do
     hour = String.rjust("#{d.hour}", 2, ?0)
     min = String.rjust("#{d.min}", 2, ?0)
     sec = String.rjust("#{d.sec}", 2, ?0)
-    "#{d.year}-#{month}-#{day}T#{hour}:#{min}:#{sec}-05:00"
+    "#{d.year}-#{month}-#{day}T#{hour}:#{min}:#{sec}Z"
   end
 end
